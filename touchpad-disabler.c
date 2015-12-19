bf7fcd9e0095b446fc206f0f0fe2dcc6577009e6
insanengineer <insanengineer@gmail.com>
2015-12-19 15:43:06 -0800

    More Updates
    Fixed a few issues
    1. File was not writing correctly
    2. App reboot did not always detect read the save file correctly
    3. Dangling pointers (oops!)
    4. Saved device file now only writes if device has changed
    
    Some code restructuring and a few locgical changes.
    Comments Comments Comments

--- touchpad-disabler.c
+++ touchpad-disabler.c
@@ -1,85 +1,172 @@
-/*This file is part of touchpad-disabler.
+/****************************************************************************
+** This file is part of touchpad-disabler.
+**
+**    Foobar is free software: you can redistribute it and/or modify
+**    it under the terms of the GNU General Public License as published by
+**    the Free Software Foundation, either version 3 of the License, or
+**    (at your option) any later version.
+**
+**    Foobar is distributed in the hope that it will be useful,
+**    but WITHOUT ANY WARRANTY; without even the implied warranty of
+**    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**    GNU General Public License for more details.
 
-    Foobar is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Foobar is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with touchpad-disabler.  If not, see <http://www.gnu.org/licenses/>.*/
+**    You should have received a copy of the GNU General Public License
+**    along with touchpad-disabler.
+**    If not, see <http://www.gnu.org/licenses/>.
+**
+****************************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 
 #include "touchpad-disabler.h"
 
-char* concat(char *string_one, char *string_two)
+/****************************************************************************
+** Function Name: get_saved_file_path
+**
+** Inputs: None
+**
+** Output: Ponter to concatenated sting of the users home directory and the
+**         saved file name.
+**
+** Description: This function get the users home directory path from the
+**		the envronment vairables. After the home directory path has
+** 		been fetched then it get concatenated with the saved device
+**		file name and then returned.
+**
+****************************************************************************/
+char *get_saved_file_path()
 {
-    size_t string_one_length = strlen(string_one);
-    size_t string_two_length = strlen(string_two);
+    char saved_file_name[] = "/.saved_device";
+    char *users_home_path;
 
-    char *result = malloc(string_one_length + string_two_length + 1);
+    users_home_path = getenv("HOME");
 
-    strcpy(result, string_one);
-    strcat(result, string_two);
+    strcat(users_home_path, saved_file_name);
+
+    return users_home_path;
+}
+
+/****************************************************************************
+** Function Name: get_saved_file_path
+**
+** Inputs: string file_path[]
+**
+** Output: A boolean value that idicates if the requested file is present or
+**         not. A value of true indicates the file exisits.
+**
+** Description: This function uses the access command to see if the selected
+**  		file exisits and is readable and writable.
+**
+****************************************************************************/
+bool check_file(char file_path[])
+{
+    int file_check_result = -1;
+    bool result = false;
+
+    // check if the file exists and is readable and writable
+    file_check_result = access(file_path, F_OK | R_OK | W_OK);
+
+    if (file_check_result == 0)
+    {
+       result = true;
+    }
 
     return result;
 }
 
-void read_saved_device(char array[])
+/****************************************************************************
+** Function Name: read_saved_device
+**
+** Inputs: string file_path[]
+**
+** Output: A pointer to a string that contains the contents of the file.
+**
+** Description: This function checks to see if the saved device file is open
+** 		then gets the data from the file and returns a pointer to it.
+**
+****************************************************************************/
+char *read_saved_device(char file_path[])
 {
     FILE *file_handle;
-    char read_buffer[MAX_BUFFER_SIZE];
+    char read_buffer[25];
+    char *read_data;
 
-    // open the saved device file as readonly
-    file_handle = fopen(saved_file_path, "r");
-
-    if (file_handle != NULL)
+    if (check_file(file_path) == true)
     {
-      fgets(read_buffer, MAX_BUFFER_SIZE, file_handle);
-      strcpy(array, read_buffer);
+    	// open the saved device file as readonly
+        file_handle = fopen(file_path, "r");
 
-      printf("%s\n", read_buffer);
+        if (file_handle != NULL)
+        {
+           fgets(read_buffer, 25, file_handle);
+           //strcpy(data, read_buffer);
 
-      fclose(file_handle);
+           read_data = read_buffer;
+
+           fclose(file_handle);
+        }
     }
+
+    return read_data;
 }
 
-void write_saved_device(char array[])
+/****************************************************************************
+** Function Name: write_saved_device
+**
+** Inputs: string data[], string file_path[]
+**
+** Output: None
+**
+** Description: This function writes the information from the data string into
+** 		the saved device file at the file path string.
+**
+****************************************************************************/
+void write_saved_device(const char data[], char file_path[])
 {
     FILE *file_handle;
 
     // open the saved device file foe writing
-    file_handle = fopen(saved_file_path, "w");
+    file_handle = fopen(file_path, "w");
 
     if (file_handle != NULL)
     {
-       fputs(array, file_handle);
+       fputs(data, file_handle);
        fclose(file_handle);
     }
 }
 
+/****************************************************************************
+** Function Name: check_for_mouse
+**
+** Inputs: udev_device dev
+**
+** Output: A boolean value if the device is a mouse or not. A value of true
+**         means the device is mouse.
+**
+** Description: This function gets the device path from the device passed
+**  		to it and checks to see if it is a mouse based off a const
+** 		string that holds the location and type of the device.
+**
+****************************************************************************/
 bool check_for_mouse(struct udev_device *dev)
 {
     bool result = false;
-    char dev_path_udev[MAX_BUFFER_SIZE];
+    const char *dev_path_udev;
+    const char *dev_path_string = "/dev/input/mouse";
     int char_match_cnt = 0;
 
+    dev_path_udev = udev_device_get_devnode(dev);
+
     // make sure that the data we are looking at is valid
-    if (udev_device_get_devnode(dev) != NULL)
+    if (dev_path_udev != NULL)
     {
         // length of the device path string
         int dev_path_string_len = strlen(dev_path_string);
-
-        // get a copy of the udev device path string
-        strcpy(dev_path_udev, udev_device_get_devnode(dev));
 
         for (int i = 0; i < dev_path_string_len; i++)
         {
@@ -91,8 +178,6 @@ bool check_for_mouse(struct udev_device *dev)
 
         if (char_match_cnt == dev_path_string_len)
         {
-            // make a copy of the device path so we know when it has been removed
-            write_saved_device(dev_path_udev);
             result = true;
         }
     }
@@ -100,14 +185,26 @@ bool check_for_mouse(struct udev_device *dev)
     return result;
 }
 
-bool check_device(struct udev_device *dev, char buffer[])
+/****************************************************************************
+** Function Name: compare_device
+**
+** Inputs: udev_device dev,  char buffer
+**
+** Output: A boolean value if the current plugged in device matched that of
+**         the device that is contained int he buffer variable. A value of
+** 	   true means the curent device matches the buffer.
+**
+** Description: This function uses strcmp to compare the buffer string to
+**  		the current device detected from udev
+**
+****************************************************************************/
+bool compare_device(struct udev_device *dev, char buffer[])
 {
     int compare = 0;
     bool result = false;
 
     if (udev_device_get_devnode(dev) != NULL)
     {
-        //printf("%s %s", udev_device_get_devnode(dev), buffer);
         compare = strcmp(udev_device_get_devnode(dev), buffer);
 
         if (compare == 0)
@@ -119,17 +216,22 @@ bool check_device(struct udev_device *dev, char buffer[])
     return result;
 }
 
+/****************************************************************************
+** Function Name: main
+**
+** Inputs: application inputs
+**
+** Output: exit condition
+**
+** Description: main
+**
+****************************************************************************/
 int main (int argc, char *argv[])
 {
-    int act_compare = 0;
-    char file_contents_buffer[MAX_BUFFER_SIZE];
-    char saved_file_name[] = "/.saved_device";
+    char *file_contents_buffer; //[25];
+    char *saved_file_path;
 
-    char* users_home_path = getenv("HOME");
-    saved_file_path = concat(users_home_path, saved_file_name);
-    //printf("%s\n", saved_file_path);
-
-    read_saved_device(file_contents_buffer);
+    saved_file_path = get_saved_file_path();
 
     // create the udev object
     udev = udev_new();
@@ -171,7 +273,9 @@ int main (int argc, char *argv[])
         {
             if (check_for_mouse(device) == true)
             {
-                if (check_device(device, file_contents_buffer) == true)
+                file_contents_buffer = read_saved_device(saved_file_path);
+
+	        if (compare_device(device, file_contents_buffer) == true)
                 {
                    // disable the touchpad since a mouse is plugged in
                    system("synclient TouchpadOff=1");
@@ -180,6 +284,9 @@ int main (int argc, char *argv[])
             }
         }
     }
+
+    free(devices);
+    free(dev_list_entry);
 
     while (1)
     {
@@ -190,44 +297,72 @@ int main (int argc, char *argv[])
 
         if (device)
         {
-            // check if a usb input device has been added
-            act_compare = strcmp(udev_device_get_action(device), dev_action_add_string);
+            const char *read_dev_path;
+	    const char *read_dev_action;
+
+            read_dev_path = udev_device_get_devnode(device);
+	    read_dev_action = udev_device_get_action(device);
 
             // a usb device has been added
-            if (act_compare == 0)
+            if (strcmp(read_dev_action, "add") == 0)
             {
                 // if the device is a mouse then disable the touchpad
                 if (check_for_mouse(device) == true)
                 {
-                    // disable the touchpad
+		    // this section of code is to avoid exessive writes of the
+		    // saved device file. The basic logic is that if the file
+		    // exists the check if the contents are the same as the
+		    // current plugged in device. If the contents to not match
+		    // current device update the file.
+		    if (check_file(saved_file_path) == true)
+		    {
+	 		 char *saved_device;
+
+		         saved_device = read_saved_device(saved_file_path);
+
+		         if (strcmp(saved_device, read_dev_path) != 0)
+			 {
+			    // write the saved device file since a device was plugged in
+		    	    write_saved_device(read_dev_path, saved_file_path);
+			 }
+
+		         free(saved_device);
+		    }
+		    else
+		    {
+			write_saved_device(read_dev_path, saved_file_path);
+		    }
+
+		    // disable the touchpad
                     system("synclient TouchpadOff=1");
                 }
             }
-            else
+
+	    // a usb device has been removed
+            if (strcmp(read_dev_action, "remove") == 0)
             {
                 // check if the device has been removed
-                act_compare = strcmp(udev_device_get_action(device), dev_action_remove_string);
-
-                if (act_compare == 0)
-                {
-                    // check if it was the same device removed
-                    if (check_device(device, file_contents_buffer) == true)
-                    {
-                        // enable the touchpad
-                        system("synclient TouchpadOff=0");
-                    }
-                }
+		if (check_for_mouse(device) == true)
+		{
+		   system("synclient TouchpadOff=0");
+		}
             }
-
-            udev_device_unref(device);
         }
+
+        // release the udev device
+        udev_device_unref(device);
 
         // 250 millisecond delay
         usleep(250 * 1000);
     }
 
-    free(saved_file_path);
     free(udev);
+    free(device_monitor);
+    free(device);
+    free(dev_enum);
+
+    free(saved_file_path);
+    free(file_contents_buffer);
 
     return 0;
 }
--- touchpad-disabler.h
+++ touchpad-disabler.h
@@ -1,24 +1,26 @@
-/*This file is part of touchpad-disabler.
+/****************************************************************************
+** This file is part of touchpad-disabler.
+**
+**    Foobar is free software: you can redistribute it and/or modify
+**    it under the terms of the GNU General Public License as published by
+**    the Free Software Foundation, either version 3 of the License, or
+**    (at your option) any later version.
+**
+**    Foobar is distributed in the hope that it will be useful,
+**    but WITHOUT ANY WARRANTY; without even the implied warranty of
+**    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**    GNU General Public License for more details.
 
-    Foobar is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Foobar is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with touchpad-disabler.  If not, see <http://www.gnu.org/licenses/>.*/
+**    You should have received a copy of the GNU General Public License
+**    along with touchpad-disabler.
+**    If not, see <http://www.gnu.org/licenses/>.
+**
+****************************************************************************/
 #ifndef _touchpad_disabler_h
 #define _touchpad_disabler_h
 
 #include <stdbool.h>
 #include <libudev.h>
-
-#define MAX_BUFFER_SIZE 25
 
 // udev structure pointers
 struct udev *udev;
@@ -28,19 +30,11 @@ struct udev_list_entry *devices;
 struct udev_list_entry *dev_list_entry;
 struct udev_device *device;
 
-// Constant strings used for comparisons
-const char dev_path_string[] = "/dev/input/mouse";
-const char dev_action_add_string[] = "add";
-const char dev_action_remove_string[] = "remove";
-
-char* saved_file_path;
-
-//const char saved_device_file_path[] = "/home/dungeonmaster/gitClones/touchpad-disabler/.saved_device";
-//const char saved_file_name[] = "/.saved_device"
-
-bool check_for_mouse(struct udev_device *dev);
+char *get_saved_file_path();
+bool check_file(char file_path[]);
 bool check_device(struct udev_device *dev, char buffer[]);
-void read_saved_device(char array[]);
-void write_saved_device(char array[]);
+bool check_for_mouse(struct udev_device *dev);
+char *read_saved_device(char file_path[]);
+void write_saved_device(const char data[], char file_path[]);
 
 #endif
